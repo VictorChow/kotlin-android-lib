e33e9afcd2b8f2ed3e549b725bfe6b9e0451a65b
Loren <dayan805@163.com>
2017-09-13 22:46:40 +0800

    add get status bar & virtual nav bar height

--- ext/src/main/java/pers/victor/ext/DisplayExt.kt
+++ ext/src/main/java/pers/victor/ext/DisplayExt.kt
@@ -1,5 +1,11 @@
 package pers.victor.ext
 
+import android.content.Context
+import android.graphics.Point
+import android.util.DisplayMetrics
+import android.view.WindowManager
+
+
 /**
  * Created by Victor on 2017/8/21. (ง •̀_•́)ง
  */
@@ -23,3 +29,25 @@ fun sp2px(sp: Number) = (sp.toFloat() * app.resources.displayMetrics.scaledDensi
 fun px2dp(px: Number) = (px.toFloat() / app.resources.displayMetrics.density + 0.5f).toInt()
 
 fun px2sp(px: Number) = (px.toFloat() / app.resources.displayMetrics.scaledDensity + 0.5f).toInt()
+
+fun getStatusBarHeight(): Int {
+    var result = 0
+    val resourceId = app.resources.getIdentifier("status_bar_height", "dimen", "android")
+    if (resourceId > 0) {
+        result = app.resources.getDimensionPixelSize(resourceId)
+    }
+    return result
+}
+
+fun getVirNavBarHeight(): Int {
+    val wm = (app.getSystemService(Context.WINDOW_SERVICE) as WindowManager)
+    val display = wm.defaultDisplay
+    val p = Point()
+    display.getSize(p)
+    val screenHeight = Math.max(p.y, p.x)
+    val dm = DisplayMetrics()
+    val c: Class<*> = Class.forName("android.view.Display")
+    val method = c.getMethod("getRealMetrics", DisplayMetrics::class.java)
+    method.invoke(display, dm)
+    return Math.max(dm.heightPixels, dm.widthPixels) - screenHeight
+}
