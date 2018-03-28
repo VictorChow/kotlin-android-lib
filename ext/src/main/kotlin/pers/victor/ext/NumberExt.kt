d4ec43392956fd8f2ae9e094365fccf33b6dd259
pdog <pdog@qq.com>
2018-03-28 11:00:14 +0800

    create NumberExt.kt

--- ext/src/main/kotlin/pers/victor/ext/NumberExt.kt
+++ ext/src/main/kotlin/pers/victor/ext/NumberExt.kt
@@ -1,13 +1,18 @@
 package pers.victor.ext
 
-fun Number.dp(): Int {
-    return px2dp(this)
-}
+/**
+ * 正常编码中一般只会用到 Number.dp()/sp() 和 Number.px() ;
+ * 其中Number.dp()/sp()会根据系统分辨率将输入的px值转换为对应的px
+ * 而Number.px()只是返回自身，目的是表明自己是px值
+ */
 
-fun Number.px(): Int {
-    return dp2px(this)
-}
+fun Number.dp() = dp2px(this)        // [xxhdpi](360 -> 1080)
 
-fun Number.sp(): Int {
-    return px2sp(this)
-}
+fun Number.sp() = sp2px(this)        // [xxhdpi](360 -> 1080)
+
+fun Number.px() = this.toInt()           // [xxhdpi](360 -> 360)
+
+
+fun Number.px2dp() = px2dp(this)     // [xxhdpi](360 -> 120)
+
+fun Number.px2sp() = px2sp(this)     // [xxhdpi](360 -> 120)
