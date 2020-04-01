cf2f404e63e86987076ab6d6a672050a7d6a2959
loren1994 <dayan805@163.com>
2020-04-01 17:45:29 +0800

    增加版本判断 & inflate

--- ext/src/main/kotlin/pers/victor/ext/ApiExt.kt
+++ ext/src/main/kotlin/pers/victor/ext/ApiExt.kt
@@ -17,3 +17,11 @@ inline fun belowApi(api: Int, included: Boolean = false, block: () -> Unit) {
         block()
     }
 }
+
+fun aboveAndroid6(): Boolean = Build.VERSION.SDK_INT >= 23
+fun aboveAndroid7(): Boolean = Build.VERSION.SDK_INT >= 24
+fun aboveAndroid7_1(): Boolean = Build.VERSION.SDK_INT >= 25
+fun aboveAndroid8(): Boolean = Build.VERSION.SDK_INT >= 26
+fun aboveAndroid8_1(): Boolean = Build.VERSION.SDK_INT >= 27
+fun aboveAndroid9(): Boolean = Build.VERSION.SDK_INT >= 28
+fun aboveAndroid10(): Boolean = Build.VERSION.SDK_INT >= 29
